1e31e2850b7b4ff753293c870665fcd6508c198b
Rafael Guibson <rafaelguibsons@gmail.com>
2024-01-10 15:24:59 -0300

    Removida a classe confid, adicionado comentário ao metodo que encontra as classes em pacotes separados

--- Frontend-migracao/Monitorador/src/main/java/wicket/classes/HomePage.java
+++ Frontend-migracao/Monitorador/src/main/java/wicket/classes/HomePage.java
@@ -1,5 +1,7 @@
 package wicket.classes;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
@@ -13,6 +15,12 @@ public class HomePage extends WebPage {
 		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
 		// TODO Add your page's components here
-
+		add(new AjaxLink<Void>("link") {
+			@Override
+			public void onClick(AjaxRequestTarget target) {
+				// Redirecionar para PageTeste
+				setResponsePage(PageTeste.class);
+			}
+		});
 	}
 }
