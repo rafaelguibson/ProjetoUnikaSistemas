c84ddbfccf04e570701148cab4c289dc7afe048d
Rafael Guibson <rafaelguibsons@gmail.com>
2024-01-25 18:00:41 -0300

    Implementada validação de registros duplicados, e excel

--- Frontend-migracao/Monitorador/src/main/java/wicket/classes/UploadError.java
+++ Frontend-migracao/Monitorador/src/main/java/wicket/classes/UploadError.java
@@ -1,12 +1,18 @@
 package wicket.classes;
 import com.amazonaws.services.dynamodbv2.xspec.M;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.extensions.ajax.markup.html.modal.ModalWindow;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.upload.FileUploadField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
@@ -28,29 +34,62 @@ public class TableListModal extends Panel {
     MonitoradorHttpClient monitoradorHttpClient = new MonitoradorHttpClient("http://localhost:8080/api/monitoradores");
     List<Monitorador> mntList = new ArrayList<>();
     FeedbackPanel fp;
-    public TableListModal(String id, List<Monitorador> ExcelMonitoradorList) {
+    public TableListModal(String id, List<Monitorador> ExcelMonitoradorList, boolean load) {
         super(id);
 
         WebMarkupContainer tableSection = new WebMarkupContainer("tableSection");
         tableSection.setOutputMarkupId(true);
-        tableSection.setVisible(true);
+        tableSection.setVisible(load);
         add(tableSection);
+
+        Form<Void> form = new Form<>("form");
+        form.setOutputMarkupId(true);
+        tableSection.add(form);
 
         fp = new FeedbackPanel("feedbackPanel");
         fp.setOutputMarkupPlaceholderTag(true);
-        add(fp);
+        tableSection.add(fp);
+
+        ListView<Monitorador> monitoradorList = getComponents(ExcelMonitoradorList);
+        form.add(monitoradorList);
 
 
+        AjaxLink<Void> btnClose = new AjaxLink<Void>("btnClose") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                ModalWindow.closeCurrent(target);
+                target.add(tableSection);
+            }
+        };
+//        btnClose.add(new AjaxFormSubmitBehavior(form,"click") {});
+        btnClose.setOutputMarkupId(true);
+        tableSection.add(btnClose);
 
+        AjaxLink<Void> btnSave = new AjaxLink<Void>("btnSave") {
+            @Override
+            public void onClick(AjaxRequestTarget target) {
+                try {
+                    monitoradorHttpClient.saveAll(ExcelMonitoradorList);
+                    fp.info("Lista Cadastrada com sucesso !");
+                } catch (Exception e) {
+                    fp.info(e.getMessage());
+                    target.add(tableSection);
+                }
 
+            }
+        };
+
+        tableSection.add(btnSave);
+
+    }
+
+    private static ListView<Monitorador> getComponents(List<Monitorador> ExcelMonitoradorList) {
         ListView<Monitorador> monitoradorList = new ListView<Monitorador>("monitoradorList", ExcelMonitoradorList) {
 
             @Override
             protected void populateItem(ListItem<Monitorador> item) {
                 Monitorador monitorador = item.getModelObject();
 
-                // Coluna do chebox para selecionar os monitoradores para deletar
-                item.add(new CheckBox("selected", new PropertyModel<>(item.getModel(), "selected")));
 
                 item.add(new Label("id", new PropertyModel<String>(item.getModel(), "id")));
 
@@ -75,72 +114,11 @@ public class TableListModal extends Panel {
 
                 String status = monitorador.getStatus().toString();
                 item.add(new Label("status", status));
+
             }
         };
         monitoradorList.setOutputMarkupId(true);
         monitoradorList.setReuseItems(true);
-
-        AjaxLink<Void> btnSelectAll = new AjaxLink<Void>("btnSelectAll") {
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-                for (Monitorador monitorador : ExcelMonitoradorList) {
-                    if(!monitorador.isSelected()) {
-                        monitorador.setSelected(true);
-                        target.add(tableSection);
-                    } else {
-                        monitorador.setSelected(false);
-                        target.add(tableSection);
-                    }
-                }
-            }
-        };
-//        btnSelectAll.add(new AjaxFormSubmitBehavior("click") {});
-        btnSelectAll.setOutputMarkupId(true);
-        tableSection.add(monitoradorList, btnSelectAll);
-
-
-        AjaxLink<Void> btnClose = new AjaxLink<Void>("btnClose") {
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-                ModalWindow.closeCurrent(target);
-                target.add(tableSection);
-            }
-        };
-//        btnClose.add(new AjaxFormSubmitBehavior(form,"click") {});
-        btnClose.setOutputMarkupId(true);
-        tableSection.add(btnClose);
-
-        AjaxLink<Void> btnDelete = new AjaxLink<Void>("btnDelete") {
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-                List<Monitorador> monitoradorRemove = mntList.stream().
-                        filter(Monitorador::isSelected).collect(Collectors.toList());
-
-                if(monitoradorRemove.isEmpty()) {
-                    info("Selecione algum registro para remover...");
-                } else {
-                    info( "Foram removidos ("+monitoradorRemove.size()+") monitoradores...");
-                }
-                target.add(tableSection);
-            }
-        };
-//        btnDelete.add(new AjaxFormSubmitBehavior(form,"click") {});
-        btnDelete.setOutputMarkupId(true);
-        tableSection.add(btnDelete);
-
-
-        AjaxLink<Void> btnSave = new AjaxLink<Void>("btnSave") {
-            @Override
-            public void onClick(AjaxRequestTarget target) {
-
-            }
-        };
-//        btnUpload.add(new AjaxFormSubmitBehavior(form,"click") {});
-        btnDelete.setOutputMarkupId(true);
-        tableSection.add(btnSave);
-
-
-
-
+        return monitoradorList;
     }
 }
