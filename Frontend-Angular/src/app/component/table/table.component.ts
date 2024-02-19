a374a36bb3c3bcec25bd23e190dd599653eaa3d4
rafaelguibson <rafaelguibsons@gmail.com>
2024-02-19 09:25:01 -0300

    Adicionado paginator a data table

--- Frontend-Angular/src/app/component/table/table.component.ts
+++ Frontend-Angular/src/app/component/table/table.component.ts
@@ -12,14 +12,19 @@ import {Monitorador} from "../../model/monitorador";
 })
 export class TableComponent {
   displayedColumns = ['id', 'nomeRazaoSocial', 'cpfCnpj', 'telefone', 'email'];
-  dataSource!:MatTableDataSource<Monitorador>
-  post:any;
+  dataSource!:MatTableDataSource<Monitorador>;
+  @ViewChild(MatPaginator) paginator!:MatPaginator
+  @ViewChild(MatSort) sort!:MatSort
+
+  data:any;
   constructor(private httpService : MonitoradorHttpClientService) {
     this.httpService.getData().subscribe(data => {
       console.log(data);
-      this.post = data;
+      this.data = data;
 
-      this.dataSource = new MatTableDataSource(this.post);
+      this.dataSource = new MatTableDataSource(this.data);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     })
   }
 
